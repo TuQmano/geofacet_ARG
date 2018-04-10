270a0c3129f6a4ea7ba9802c361037598a4532c6
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-10 16:49:26 -0500

    Create geofacet_SantaFe.R

---
+++ SANTA_FE/geofacet_SantaFe.R
@@ -0,0 +1,10 @@
+
+## GRID SANTA FE
+
+SantaFe <- data.frame(
+  name = c("General Obligado", "Nueve de Julio", "Vera", "San Javier", "San Cristobal", "San Justo", "Garay", "Castellanos", "Las Colonias", "La Capital", "San Jeronimo", "San Martin", "San Lorenzo", "Belgrano", "Iriondo", "Rosario", "Caseros", "Constitucion", "General Lopez"),
+  code = c("General Obligado", "Nueve de Julio", "Vera", "San Javier", "San Cristobal", "San Justo", "Garay", "Castellanos", "Las Colonias", "La Capital", "San Jeronimo", "San Martin", "San Lorenzo", "Belgrano", "Iriondo", "Rosario", "Caseros", "Constitucion", "General Lopez"),
+  row = c(2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7, 8),
+  col = c(3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 2, 1, 4, 1, 2, 3, 1, 2, 1),
+  stringsAsFactors = FALSE
+)
