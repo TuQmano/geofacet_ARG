37ce7d1b0bc7490397653670cf0951ea172ce0eb
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-10 09:57:58 -0500

    Create PBA_geofacet.R

---
+++ PBA/PBA_geofacet.R
@@ -0,0 +1,9 @@
+
+
+PBA_grid <- data.frame(
+  name = c("Segunda", "Primera", "Tercera", "Capital", "Séptima", "Cuarta", "Quinta", "Sexta"),
+  code = c(" 2", " 1", " 3", " CAP", " 7", " 4", " 5", " 6"),
+  row = c(1, 1, 1, 2, 2, 2, 2, 3),
+  col = c(2, 3, 4, 4, 2, 1, 3, 1),
+  stringsAsFactors = FALSE
+)
