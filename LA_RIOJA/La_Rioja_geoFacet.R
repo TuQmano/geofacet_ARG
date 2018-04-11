c9c8142d8a3187f2a4b01a4c65b4ef68f88929a5
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-11 16:45:47 -0500

    Create La_Rioja_geoFacet.R

---
+++ LA_RIOJA/La_Rioja_geoFacet.R
@@ -0,0 +1,9 @@
+#GeoFacet Departamentos de la provincia de La Rioja (ARGENTINA)
+
+LaRioja <- data.frame(
+  name = c("Arauco", "Castro Barros", "Famatina", "General Lamadrid", "San Blas de los Sauces", "Vinchina", "Capital", "Chilecito", "Coronel Felipe Varela", "Sanagasta", "Chamical", "General angel V. Peñaloza*", "Independencia", "General Belgrano", "General Juan F. Quiroga", "General Ocampo", "Rosario Vera Peñaloza", "General San Martin"),
+  code = c("Arauco", "Castro Barros", "Famatina", "General Lamadrid", "San Blas de los Sauces", "Vinchina", "Capital", "Chilecito", "Coronel Felipe Varela", "Sanagasta", "Chamical", "General angel V. Peñaloza*", "Independencia", "General Belgrano", "General Juan F. Quiroga", "General Ocampo", "Rosario Vera Peñaloza", "General San Martin"),
+  row = c(1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6),
+  col = c(6, 5, 3, 1, 4, 2, 6, 4, 3, 5, 6, 5, 4, 6, 5, 6, 5, 6),
+  stringsAsFactors = FALSE
+)
