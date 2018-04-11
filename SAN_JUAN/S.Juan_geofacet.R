87dbde59fc260b0363a060e54bd540ace477dbab
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-11 17:09:27 -0500

    Create S.Juan_geofacet.R

---
+++ SAN_JUAN/S.Juan_geofacet.R
@@ -0,0 +1,9 @@
+#GeoFacet departamentos provincia de San Juan (ARGENTINA)
+
+SJuan <- data.frame(
+  name = c("Iglesia", "Jáchal", "Valle Fértil", "Ullum", "Albardón", "Angaco", "Zonda", "San Martín", "Caucete", "Rivadavia", "Chimbas", "San Lucía", "Pocito", "Capital", "9 de Julio", "Calingasta", "Rawson", "25 de Mayo", "Sarmiento"),
+  code = c("Iglesia", "Jáchal", "Valle Fértil", "Ullum", "Albardón", "Angaco", "Zonda", "San Martín", "Caucete", "Rivadavia", "Chimbas", "San Lucía", "Pocito", "Capital", "10 de Julio", "Calingasta", "Rawson", "26 de Mayo", "Sarmiento"),
+  row = c(2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 8),
+  col = c(2, 3, 4, 3, 4, 5, 3, 4, 5, 3, 4, 5, 3, 4, 5, 2, 4, 5, 4),
+  stringsAsFactors = FALSE
+)
