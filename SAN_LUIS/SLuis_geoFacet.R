e1873b179010429e491de909d407d99d7ff35dc2
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-11 17:32:09 -0500

    Create SLuis_geoFacet.R

---
+++ SAN_LUIS/SLuis_geoFacet.R
@@ -0,0 +1,9 @@
+# GeoFacet departamentos de la provincia de San Luis (ARGENTINA)
+
+SLuis <- data.frame(
+  name = c("Ayacucho", "Junin", "Belgrano", "Chacabuco", "Libertador General San Martin", "Coronel Pringles", "General Pedernera", "Juan Martin de Pueyrredon", "Gobernador Dupuy"),
+  code = c("Ayacucho", "Junin", "Belgrano", "Chacabuco", "Libertador General San Martin", "Coronel Pringles", "General Pedernera", "Juan Martin de Pueyrredon", "Gobernador Dupuy"),
+  row = c(1, 1, 2, 2, 2, 3, 3, 3, 4),
+  col = c(2, 3, 1, 3, 2, 2, 3, 1, 3),
+  stringsAsFactors = FALSE
+)
