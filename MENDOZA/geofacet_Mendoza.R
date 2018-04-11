83eb05b8d2442a95a1bb6792ac387d8aaf79a5b2
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-11 08:59:33 -0500

    Create geofacet_Mendoza.R

---
+++ MENDOZA/geofacet_Mendoza.R
@@ -0,0 +1,9 @@
+#### MENDOZA GRIDA - GeoFacet
+
+Mendoza_grid <- data.frame(
+  name = c("Las Heras", "Lavalle", "Guaymallén", "Godoy Cruz", "Lujan de Cuyo", "Capital", "Maipu", "San Martin", "Tupungato", "Junin", "Tunuyan", "La Paz", "Santa Rosa", "Rivadavia", "San Carlos", "General Alvear", "San Rafael", "Malargue"),
+  code = c("Las Heras", "Lavalle", "Guaymallén", "Godoy Cruz", "Lujan de Cuyo", "Capital", "Maipu", "San Martin", "Tupungato", "Junin", "Tunuyan", "La Paz", "Santa Rosa", "Rivadavia", "San Carlos", "General Alvear", "San Rafael", "Malargue"),
+  row = c(1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6),
+  col = c(3, 4, 4, 3, 1, 2, 3, 5, 2, 4, 2, 5, 4, 3, 2, 4, 3, 1),
+  stringsAsFactors = FALSE
+)
