a7c4437fdbff01392ec308d3efa16747502dd6aa
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-12 16:45:10 -0500

    Create geofacet_jujuy.R

---
+++ JUJUY/geofacet_jujuy.R
@@ -0,0 +1,11 @@
+#GeoFacet departamentos de la provincia de Jujuy
+
+# GRILLA
+
+jujuy_grid <- data.frame(
+  name = c("Yavi", "Santa Catalina", "Humahuaca", "Rinconada", "Cochinoca", "Valle Grande", "Ledesma", "Susques", "Tumbaya", "Tilcara", "Santa Bárbara", "Dr. Manuel Belgrano", "Palpalá", "San Pedro", "San Antonio", "El Carmen"),
+  code = c("Yavi", "Santa Catalina", "Humahuaca", "Rinconada", "Cochinoca", "Valle Grande", "Ledesma", "Susques", "Tumbaya", "Tilcara", "Santa Bárbara", "Dr. Manuel Belgrano", "Palpalá", "San Pedro", "San Antonio", "El Carmen"),
+  row = c(2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6),
+  col = c(3, 2, 3, 1, 2, 4, 5, 1, 3, 4, 5, 3, 4, 5, 3, 4),
+  stringsAsFactors = FALSE
+)
