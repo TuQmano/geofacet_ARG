d173fb9cda97ff44737ed3c4fbcb17f77ddc186e
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-12 13:18:32 -0500

    Create NQN_geofacet.R

---
+++ NEUQUEN/NQN_geofacet.R
@@ -0,0 +1,9 @@
+### GEOFACET NEUQUEN POR DEPARTAMENTO
+
+nqn_grid <- data.frame(
+  name = c("Chos Malal", "Minas", "Pehuenches", "Añelo", "Loncopué", "Ñorquín", "Confluencia", "Picunches", "Zapala", "Alumine", "Catán Lil", "Picún Leufú", "Collón Curá", "Huiliches", "Lácar", "Los Lagos"),
+  code = c("Chos Malal", "Minas", "Pehuenches", "Añelo", "Loncopué", "Ñorquín", "Confluencia", "Picunches", "Zapala", "Alumine", "Catán Lil", "Picún Leufú", "Collón Curá", "Huiliches", "Lácar", "Los Lagos"),
+  row = c(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 7),
+  col = c(3, 2, 4, 4, 3, 2, 4, 2, 3, 1, 2, 3, 2, 1, 1, 1),
+  stringsAsFactors = FALSE
+)
