09d17d1a540b52fbcc100098052935f02ce6b7a0
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-12 10:09:17 -0500

    Delete r.negro_geofacet.R

--- RIO_NEGROR/r.negro_geofacet.R
+++
@@ -1,9 +0,0 @@
-# GRILLA DEPARTAMENTOS DE RIO NEGRO
-
-r.negro_grid <- data.frame(
-  name = c("General Roca", "Avellaneda", "El Cuy", "Pichi Mahuida", "Pilcaniyeu", "Conesa", "Nueve de Julio", "Valcheta", "Veinticinco de Mayo", "Ñorquincó", "Bariloche", "San Antonio", "Adolfo Alsina"),
-  code = c("General Roca", "Avellaneda", "El Cuy", "Pichi Mahuida", "Pilcaniyeu", "Conesa", "Nueve de Julio", "Valcheta", "Veinticinco de Mayo", "Ñorquincó", "Bariloche", "San Antonio", "Adolfo Alsina"),
-  row = c(1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4),
-  col = c(3, 4, 3, 5, 2, 6, 4, 5, 3, 2, 1, 6, 7),
-  stringsAsFactors = FALSE
-)
