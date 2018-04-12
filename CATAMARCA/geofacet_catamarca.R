f6e4a1404fc62332a820a476f972a3fd304f80fa
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-12 17:10:44 -0500

    Create geofacet_catamarca.R

---
+++ CATAMARCA/geofacet_catamarca.R
@@ -0,0 +1,10 @@
+# GeoFacet departamentos de CATAMARCA
+
+catamarca_grid <- data.frame(
+  name = c("Antofagasta de la Sierra", "Santa María", "Belén", "Tinogasta", "Andalgalá", "Paclín", "Santa Rosa", "Ambato", "Pomán", "El Alto", "Fray Mamerto Esquiú", "Capital", "Ancasti", "Valle Viejo", "Capayán", "La Paz"),
+  code = c("Antofagasta de la Sierra", "Santa María", "Belén", "Tinogasta", "Andalgalá", "Paclín", "Santa Rosa", "Ambato", "Pomán", "El Alto", "Fray Mamerto Esquiú", "Capital", "Ancasti", "Valle Viejo", "Capayán", "La Paz"),
+  row = c(1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6),
+  col = c(1, 3, 2, 1, 3, 5, 6, 4, 3, 6, 5, 4, 6, 5, 4, 7),
+  stringsAsFactors = FALSE
+)
+geofacet::grid_preview(mygrid)
