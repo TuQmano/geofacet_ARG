75aaa73cae2c7766a466bd7289f1e5b1f09bad94
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-12 10:10:42 -0500

    Create salta_geofacet.R

---
+++ SALTA/salta_geofacet.R
@@ -0,0 +1,9 @@
+### GRILLA DEPARTAMENTOS DE LA PROVINCIA DE SALTA 
+
+salta-grid <- data.frame(
+  name = c("Santa Victoria", "Iruya", "General José de San Martín", "Orán", "La Poma", "General Guemes", "Rivadavia", "La Caldera", "Rosario de Lerma", "Los Andes", "Capital", "Anta", "Cerrillos", "Cachi", "Metán", "Chicoana", "Molinos", "San Carlos", "Rosario de la Frontera", "La Viña", "Guachipas", "La Candelaria", "Cafayate"),
+  code = c("Santa Victoria", "Iruya", "General José de San Martín", "Orán", "La Poma", "General Guemes", "Rivadavia", "La Caldera", "Rosario de Lerma", "Los Andes", "Capital", "Anta", "Cerrillos", "Cachi", "Metán", "Chicoana", "Molinos", "San Carlos", "Rosario de la Frontera", "La Viña", "Guachipas", "La Candelaria", "Cafayate"),
+  row = c(1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7),
+  col = c(5, 6, 7, 6, 2, 5, 7, 4, 3, 2, 5, 6, 4, 3, 5, 4, 2, 3, 6, 4, 5, 5, 4),
+  stringsAsFactors = FALSE
+)
