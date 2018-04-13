d6b2322c6a2357064abe34b817b45e7af85352c7
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-13 09:49:35 -0500

    Create geofacet_formosa.R

---
+++ FORMOSA/geofacet_formosa.R
@@ -0,0 +1,9 @@
+# GRILLA DE DEPARTAMENTOS DE LA PROVINCIA DE FORMOSA
+
+formosa_grid <- data.frame(
+  name = c("Ramón Lista", "Bermejo", "Matacos", "name", "Pilagás", "Pilcomayo", "Formosa", "Pirané", "Laishí"),
+  code = c("Ramón Lista", "Bermejo", "Matacos", "Patiño", "Pilagás", "Pilcomayo", "Formosa", "Pirané", "Laishí"),
+  row = c(1, 2, 2, 2, 2, 2, 3, 3, 4),
+  col = c(1, 2, 1, 3, 4, 5, 5, 4, 5),
+  stringsAsFactors = FALSE
+)
