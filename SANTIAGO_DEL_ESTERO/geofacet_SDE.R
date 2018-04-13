817f1e87e5032d2510bfb0b8fefdb4ff8c2bb320
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-13 10:08:22 -0500

    Create geofacet_SDE.R

---
+++ SANTIAGO_DEL_ESTERO/geofacet_SDE.R
@@ -0,0 +1,9 @@
+# Geofacet de departamentos de SANTIAGO DEL ESTERO 
+
+sde_grid <- data.frame(
+  name = c("Copo", "Pellegrini", "Alberdi", "Jiménez", "Banda", "Figueroa", "Moreno", "Río Hondo", "Capital", "Juan F. Ibarra", "Robles", "Sarmiento", "Guasayán", "Avellaneda", "Belgrano", "San Martín", "Silípica", "Choya", "Atamisqui", "Belgrano", "Loreto", "Salavina", "Aguirre", "Ojo de Agua", "Quebrachos", "Mitre", "Rivadavia"),
+  code = c("Copo", "Pellegrini", "Alberdi", "Jiménez", "Banda", "Figueroa", "Moreno", "Río Hondo", "Capital", "Juan F. Ibarra", "Robles", "Sarmiento", "Guasayán", "Avellaneda", "General Taboada", "San Martín", "Silípica", "Choya", "Atamisqui", "Belgrano", "Loreto", "Salavina", "Aguirre", "Ojo de Agua", "Quebrachos", "Mitre", "Rivadavia"),
+  row = c(1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8),
+  col = c(4, 3, 4, 3, 3, 4, 5, 2, 2, 5, 3, 4, 1, 4, 5, 3, 2, 1, 3, 5, 2, 4, 5, 2, 3, 4, 5),
+  stringsAsFactors = FALSE
+)
