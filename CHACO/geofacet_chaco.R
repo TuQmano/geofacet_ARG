cac9ef901cb7021d6088971475ded5088752c88a
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-04-13 09:31:17 -0500

    Create geofacet_chaco.R

---
+++ CHACO/geofacet_chaco.R
@@ -0,0 +1,9 @@
+### Grilla de departamentos de la provincia de Chaco
+
+chaco_grid <- data.frame(
+  name = c("General Güemes", "Almirante Brown", "Libertador General San Martín", "Nueve de Julio", "Sargento Cabral", "Quitilipi", "Maipú", "General Belgrano", "Independencia", "Bermejo", "Chacabuco", "General Donovan", "Veinticinco de Mayo", "O'Higgins", "Primero de Mayo", "Doce de Octubre", "Libertad", "Comandante Fernández", "Presidencia de la Plaza", "Mayor Luis Jorge Fontana", "San Fernando", "Fray Justo Santa María de Oro", "San Lorenzo", "Tapenagá"),
+  code = c("General Güemes", "Almirante Brown", "Libertador General San Martín", "Nueve de Julio", "Sargento Cabral", "Quitilipi", "Maipú", "General Belgrano", "Independencia", "Bermejo", "Chacabuco", "General Donovan", "Veinticinco de Mayo", "O'Higgins", "Primero de Mayo", "Doce de Octubre", "Libertad", "Comandante Fernández", "Presidencia de la Plaza", "Mayor Luis Jorge Fontana", "San Fernando", "Fray Justo Santa María de Oro", "San Lorenzo", "Tapenagá"),
+  row = c(2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6),
+  col = c(2, 1, 4, 2, 5, 4, 3, 3, 4, 7, 2, 6, 5, 3, 7, 2, 6, 4, 5, 3, 6, 2, 4, 5),
+  stringsAsFactors = FALSE
+)
