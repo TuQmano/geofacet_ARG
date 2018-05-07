5161d3339d8ba3bc8a495fa39d6e1d0c2bfe33f7
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-07 12:28:36 -0300

    Create chubut_grird.R

---
+++ CHUBUT/chubut_grird.R
@@ -0,0 +1,7 @@
+mygrid <- data.frame(
+  name = c("Biedma", "Gastre", "Telsen", "Cushamen", "Futaleufú", "Gaiman", "Rawson", "Languiñeo", "Mártires", "Paso de Indios", "Tehuelches", "Florentino Ameghino", "Río Senguer", "Escalante", "Sarmiento"),
+  code = c("Biedma", "Gastre", "Telsen", "Cushamen", "Futaleufú", "Gaiman", "Rawson", "Languiñeo", "Mártires", "Paso de Indios", "Tehuelches", "Florentino Ameghino", "Río Senguer", "Escalante", "Sarmiento"),
+  row = c(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4),
+  col = c(5, 3, 4, 2, 1, 3, 4, 2, 3, 2, 1, 4, 1, 3, 2),
+  stringsAsFactors = FALSE
+)
