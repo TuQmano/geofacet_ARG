cff5bd76e9737f31ed284e111bb365ed08ba49e7
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-07 14:57:29 -0300

    Create geofacet_LaPampa.R

---
+++ LA_PAMPA/geofacet_LaPampa.R
@@ -0,0 +1,8 @@
+# Grilla Mapa de departamentos de la provincia de La Pampa
+mygrid <- data.frame(
+  name = c("Chapaleufú", "Realicó", "Maracó", "Rancul", "Trenel", "Conhelo", "Quemú Quemú", "Capital", "Catriló", "Chalileo", "Chical Co", "Loventué", "Toay", "Atreucó", "Guatraché", "Limay Mahuida", "Puelén", "Utracán", "Curacó", "Hucal", "Lihuel Calel", "Caleu Caleu"),
+  code = c("Chapaleufú", "Realicó", "Maracó", "Rancul", "Trenel", "Conhelo", "Quemú Quemú", "Capital", "Catriló", "Chalileo", "Chical Co", "Loventué", "Toay", "Atreucó", "Guatraché", "Limay Mahuida", "Puelén", "Utracán", "Curacó", "Hucal", "Lihuel Calel", "Caleu Caleu"),
+  row = c(1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 7),
+  col = c(6, 5, 6, 4, 5, 5, 6, 5, 6, 2, 1, 3, 4, 5, 6, 3, 2, 4, 4, 6, 5, 6),
+  stringsAsFactors = FALSE
+)
