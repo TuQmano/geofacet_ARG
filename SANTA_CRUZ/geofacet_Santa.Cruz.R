6f2b6741450a4bf5af18030599aa6a19290d93cb
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-07 15:14:04 -0300

    Create geofacet_Santa.Cruz.R

---
+++ SANTA_CRUZ/geofacet_Santa.Cruz.R
@@ -0,0 +1,9 @@
+# Grilla Mapa de departamentos de la provincia de Santa Cruz
+
+mygrid <- data.frame(
+  name = c("Deseado", "Lago Buenos Aires", "Magallanes", "Río Chico", "Corpen Aike", "Lago Argentino", "Güer Aike"),
+  code = c("Deseado", "Lago Buenos Aires", "Magallanes", "Río Chico", "Corpen Aike", "Lago Argentino", "Güer Aike"),
+  row = c(1, 1, 2, 2, 3, 3, 4),
+  col = c(2, 1, 2, 1, 2, 1, 2),
+  stringsAsFactors = FALSE
+)
