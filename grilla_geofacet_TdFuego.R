aeb6c778a0289448ddcbdbf7644ecdcfae36905b
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-07 13:56:08 -0300

    Create grilla_geofacet_TdFuego.R

---
+++ grilla_geofacet_TdFuego.R
@@ -0,0 +1,9 @@
+# Grilla de departamentos de Tierra del Fuego
+
+mygrid <- data.frame(
+  name = c("Río Grande", "Tolhuin", "Ushuaia", "Antártida Argentina"),
+  code = c("Río Grande", "Tolhuin", "Ushuaia", "Antártida Argentina"),
+  row = c(2, 3, 4, 6),
+  col = c(1, 2, 1, 3),
+  stringsAsFactors = FALSE
+)
