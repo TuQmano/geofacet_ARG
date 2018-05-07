6677d1181641e88f942927667166075dd0d17c7a
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-07 15:47:17 -0300

    Create geofacet_grilla_Misiones.R

---
+++ MISIONES/geofacet_grilla_Misiones.R
@@ -0,0 +1,8 @@
+# Grilla Mapa de departamentos de la provincia de Misiones
+mygrid <- data.frame(
+  name = c("Iguazú", "General Manuel Belgrano", "El Dorado", "Libertador Gral. San Martín", "San Pedro", "Montecarlo", "San Ignacio", "Guaraní", "Cainguás", "Oberá", "Candelaria", "25 de Mayo", "Capital", "Leandro N. Alem", "San Javier", "Concepción", "Apóstoles"),
+  code = c("Iguazú", "General Manuel Belgrano", "Eldorado", "Libertador Gral. San Martín", "San Pedro", "Montecarlo", "San Ignacio", "Guaraní", "Cainguás", "Oberá", "Candelaria", "25 de Mayo", "Capital", "Leandro N. Alem", "San Javier", "Concepción", "Apóstoles"),
+  row = c(1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6),
+  col = c(5, 6, 5, 4, 6, 5, 3, 5, 4, 3, 2, 4, 1, 2, 3, 2, 1),
+  stringsAsFactors = FALSE
+)
