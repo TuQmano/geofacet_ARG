bdb5f75305420878862af0c023d82e4301988e32
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-07 16:03:43 -0300

    Create geofacet_E.Rios.R

---
+++ ENTRE_RIOS/geofacet_E.Rios.R
@@ -0,0 +1,8 @@
+# Grilla Mapa de departamentos de la provincia de Entre Ríos
+
+mygrid <- data.frame(
+  name = c("Federación", "Feliciano", "Concordia", "Federal", "La Paz", "Colón", "Paraná", "San Salvador", "Villaguay", "Diamante", "Nogoyá", "Tala", "Uruguay", "Gualeguay", "Gualeguaychú", "Victoria", "Islas del Ibicuy"),
+  code = c("Federación", "Feliciano", "Concordia", "Federal", "La Paz", "Colón", "Paraná", "San Salvador", "Villaguay", "Diamante", "Nogoyá", "Tala", "Uruguay", "Gualeguay", "Gualeguaychú", "Victoria", "Islas del Ibicuy"),
+  row = c(1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6),
+  col = c(4, 3, 4, 3, 2, 4, 1, 3, 2, 1, 2, 3, 4, 3, 4, 2, 4),
+  stringsA
