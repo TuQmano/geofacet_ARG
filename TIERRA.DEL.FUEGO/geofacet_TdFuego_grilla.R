64bae70a5a716bace86923d1117c83b06af78c1b
Juan Pablo Ruiz Nicolini <zurdopilon@gmail.com>
2018-05-14 23:39:05 -0300

    Update geofacet_TdFuego_grilla.R

--- TIERRA.DEL.FUEGO/geofacet_TdFuego_grilla.R
+++ TIERRA.DEL.FUEGO/geofacet_TdFuego_grilla.R
@@ -7,3 +7,14 @@ mygrid <- data.frame(
   col = c(1, 2, 1, 3),
   stringsAsFactors = FALSE
 )
+
+
+# Grilla de departamentos de Tierra del Fuego V2 (triangulo)
+
+mygrid <- data.frame(
+  name = c("Río Grande", "Tolhuin", "Ushuaia", "Antártida Argentina"),
+  code = c("Río Grande", "Tolhuin", "Ushuaia", "Antártida Argentina"),
+  row = c(2, 3, 4, 6),
+  col = c(1, 1, 2, 3),
+  stringsAsFactors = FALSE
+)
