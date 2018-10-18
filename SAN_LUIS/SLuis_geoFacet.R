49b9ffaa3947a05bdb2929b615cb3dec4b706251
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2018-10-18 12:20:10 -0300

    Update SLuis_geoFacet.R

--- SAN_LUIS/SLuis_geoFacet.R
+++ SAN_LUIS/SLuis_geoFacet.R
@@ -1,9 +1,13 @@
 # GeoFacet departamentos de la provincia de San Luis (ARGENTINA)
 
 SLuis <- data.frame(
-  name = c("Ayacucho", "Junin", "Belgrano", "Chacabuco", "Libertador General San Martin", "Coronel Pringles", "General Pedernera", "Juan Martin de Pueyrredon", "Gobernador Dupuy"),
-  code = c("Ayacucho", "Junin", "Belgrano", "Chacabuco", "Libertador General San Martin", "Coronel Pringles", "General Pedernera", "Juan Martin de Pueyrredon", "Gobernador Dupuy"),
-  row = c(1, 1, 2, 2, 2, 3, 3, 3, 4),
-  col = c(2, 3, 1, 3, 2, 2, 3, 1, 3),
-  stringsAsFactors = FALSE
-)
+      name = c("Ayacucho", "Junín", "Belgrano", "Chacabuco",
+               "San Martin", "Pringles", "Pedernera",
+              "Pueyrredon", "Gobernador Dupuy"),
+      code = c("007", "006", "008", "004",  # Codigos utilizados por INDRA en bases de datos electorales
+               "005", "002", "003",
+               "001", "009"),
+      row = c(1, 1, 2, 2, 2, 3, 3, 3, 4),
+      col = c(2, 3, 1, 3, 2, 2, 3, 1, 3),
+      stringsAsFactors = FALSE
+    )
