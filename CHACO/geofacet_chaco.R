67829e0ca3f8d1a7ec79f8c9b62471cbf3de4053
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2019-01-18 13:26:36 -0300

    Update geofacet_chaco.R

--- CHACO/geofacet_chaco.R
+++ CHACO/geofacet_chaco.R
@@ -1,15 +1,16 @@
 chaco_grid <- data.frame(
-  name = c("Gral Güemes", "Ate Brown", "Gral S.Martín", "9 de Julio", 
-           "Sgto Cabral", "Quitilipi", "Maipú", "Gral. Belgrano", "Independencia", "Bermejo", 
-           "Chacabuco", "Gral Donovan", "25 de Mayo", "O'Higgins", "1 de Mayo",
-           "12 de Octubre", "Libertad", "Cmdte Fernández", "Pres. de la Plaza",
-           "Mayor L.J. Fontana", "S. Fernando", "Fray J.S.M.de  Oro", "S.Lorenzo", "Tapenagá"),
-  code = c("025", "024", "008", "019",      #### Codigo de Departamento que utiliza INDRA para resultados electorales (coddepto)
-           "005", "014", "016", "018", "017", "007",
-           "020", "004", "015", "012", "002",
-           "021", "003", "013", "006", 
-           "011", "001", "023", "010", "009"),
-  row = c(2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6),
-  col = c(2, 1, 4, 2, 5, 4, 3, 3, 4, 7, 2, 6, 5, 3, 7, 2, 6, 4, 5, 3, 6, 2, 4, 5),
-  stringsAsFactors = FALSE
-)
+   name = c("Gral Guemes", "Gral S.Martín", "Ate Brown",
+            "Gral.Belgrano", "Maipú", "Quitilipi", "Sgto Cabral",
+            "25 de Mayo", "9 de Julio", "Independencia", "Bermejo", 
+            "Gral Donovan", "Cmdte Fernández", "Chacabuco", "O'Higgins", 
+            "1 de Mayo", "12 de Octubre", "Libertad", "Pres. de la Plaza",
+            "S.Lorenzo", "2 de abril", "Mayor L.J. Fontana", "S.Fernando", 
+            "Fray J.S.M.de Oro", "Tapenagá"),
+   code = c("025", "008", "024", "018", "016", "014", "005", 
+            "015", "019", "017", "007", "004", "013", "020", 
+            "012", "002", "021", "003", "006", "022", "010", 
+            "011", "001", "023", "009"),
+   row = c(2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6),
+   col = c(3, 4, 1, 2, 3, 4, 5, 5, 2, 3, 7, 6, 4, 1, 3, 7, 1, 6, 5, 4, 2, 4, 6, 3, 5),
+   stringsAsFactors = FALSE
+ )
