f0df81bcbfe7b1f38d841b49c2c10624a189c0f9
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2019-02-27 11:18:42 -0300

    Update geofacet_Mendoza.R

--- MENDOZA/geofacet_Mendoza.R
+++ MENDOZA/geofacet_Mendoza.R
@@ -1,9 +1,15 @@
 #### MENDOZA GRIDA - GeoFacet
 
-Mendoza_grid <- data.frame(
-  name = c("Las Heras", "Lavalle", "Guaymallén", "Godoy Cruz", "Lujan de Cuyo", "Capital", "Maipu", "San Martin", "Tupungato", "Junin", "Tunuyan", "La Paz", "Santa Rosa", "Rivadavia", "San Carlos", "General Alvear", "San Rafael", "Malargue"),
-  code = c("Las Heras", "Lavalle", "Guaymallén", "Godoy Cruz", "Lujan de Cuyo", "Capital", "Maipu", "San Martin", "Tupungato", "Junin", "Tunuyan", "La Paz", "Santa Rosa", "Rivadavia", "San Carlos", "General Alvear", "San Rafael", "Malargue"),
-  row = c(1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6),
-  col = c(3, 4, 4, 3, 1, 2, 3, 5, 2, 4, 2, 5, 4, 3, 2, 4, 3, 1),
+ar_mendoza_grid <- data.frame(
+  name = c("Las Heras", "Lavalle", "Guaymallén", "Lujan de Cuyo", "Capital", 
+           "San Martin", "Junin", "Godoy Cruz", "Maipu", "Tupungato", 
+           "Rivadavia", "Tunuyan", "Santa Rosa", "La Paz", "San Rafael",
+           "San Carlos", "General Alvear", "Malargue"),
+  code = c("004", "005", "003", "007", "001",
+           "009", "010", "002", "006", "008", 
+           "011", "012", "014", "015", "016", 
+           "013", "018", "017"),
+  row = c(1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 6),
+  col = c(2, 4, 3, 1, 2, 4, 4, 2, 3, 2, 3, 1, 4, 5, 3, 2, 4, 2),
   stringsAsFactors = FALSE
 )
