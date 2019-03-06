90e30a94a08b3a40f260251b4efbb7a334fd7669
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2019-03-06 12:45:47 -0300

    Update geofacet_E.Rios.R

--- ENTRE_RIOS/geofacet_E.Rios.R
+++ ENTRE_RIOS/geofacet_E.Rios.R
@@ -1,8 +1,13 @@
-# Grilla Mapa de departamentos de la provincia de Entre Ríos
+# Grilla Mapa de departamentos de la provincia de Entre Ríos (CODE corresponde al código de INDRA)
 
-mygrid <- data.frame(
-  name = c("Federación", "Feliciano", "Concordia", "Federal", "La Paz", "Colón", "Paraná", "San Salvador", "Villaguay", "Diamante", "Nogoyá", "Tala", "Uruguay", "Gualeguay", "Gualeguaychú", "Victoria", "Islas del Ibicuy"),
-  code = c("Federación", "Feliciano", "Concordia", "Federal", "La Paz", "Colón", "Paraná", "San Salvador", "Villaguay", "Diamante", "Nogoyá", "Tala", "Uruguay", "Gualeguay", "Gualeguaychú", "Victoria", "Islas del Ibicuy"),
+grilla_ER <- data.frame(
+  name = c("Federación", "Feliciano", "Concordia", "Federal", "La Paz",
+           "Colón", "Paraná", "San Salvador", "Villaguay", "Diamante", 
+           "Nogoyá", "Tala", "Uruguay", "Gualeguay", "Gualeguaychú", "Victoria", "Islas del Ibicuy"),
+  code = c("028", "016", "013", "014", "003",
+           "015", "001", "018", "012", "004",
+           "006", "008", "009", "007", "010", "005", "011"),
   row = c(1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6),
   col = c(4, 3, 4, 3, 2, 4, 1, 3, 2, 1, 2, 3, 4, 3, 4, 2, 4),
-  stringsA
+  stringsAsFactors = FALSE
+)
