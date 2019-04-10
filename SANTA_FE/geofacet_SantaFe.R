ec711b5c19305dd04b082994b770e47c366f507a
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2019-04-10 15:59:39 -0300

    Update geofacet_SantaFe.R

--- SANTA_FE/geofacet_SantaFe.R
+++ SANTA_FE/geofacet_SantaFe.R
@@ -1,9 +1,14 @@
-
-## GRID SANTA FE
-
-SantaFe <- data.frame(
-  name = c("General Obligado", "Nueve de Julio", "Vera", "San Javier", "San Cristobal", "San Justo", "Garay", "Castellanos", "Las Colonias", "La Capital", "San Jeronimo", "San Martin", "San Lorenzo", "Belgrano", "Iriondo", "Rosario", "Caseros", "Constitucion", "General Lopez"),
-  code = c("General Obligado", "Nueve de Julio", "Vera", "San Javier", "San Cristobal", "San Justo", "Garay", "Castellanos", "Las Colonias", "La Capital", "San Jeronimo", "San Martin", "San Lorenzo", "Belgrano", "Iriondo", "Rosario", "Caseros", "Constitucion", "General Lopez"),
+sfe_grid <- data.frame(
+  name = c("General Obligado", "9 de Julio", "Vera", 
+           "San Javier", "San Cristobal", "San Justo", "Garay", 
+           "Castellanos", "Las Colonias", "La Capital", "San Jeronimo",
+           "San Martin", "San Lorenzo", "Belgrano", "Iriondo",
+           "Rosario", "Caseros", "Constitucion", "General Lopez"),
+  code = c("007", "012", "022", 
+           "017", "016", "019", "005", 
+           "003", "011", "009", "018",
+           "021", "020", "001", "008",
+           "013", "002", "004", "006"),
   row = c(2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7, 8),
   col = c(3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 2, 1, 4, 1, 2, 3, 1, 2, 1),
   stringsAsFactors = FALSE
