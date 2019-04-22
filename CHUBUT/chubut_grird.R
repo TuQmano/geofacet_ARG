9ba02a6d8da3b708964fc17ae596803260fd15c6
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2019-04-22 10:53:13 -0300

    Update chubut_grird.R

--- CHUBUT/chubut_grird.R
+++ CHUBUT/chubut_grird.R
@@ -2,8 +2,15 @@
 
 
 mygrid <- data.frame(
-  name = c("Biedma", "Gastre", "Telsen", "Cushamen", "Futaleufú", "Gaiman", "Rawson", "Languiñeo", "Mártires", "Paso de Indios", "Tehuelches", "Florentino Ameghino", "Río Senguer", "Escalante", "Sarmiento"),
-  code = c("Biedma", "Gastre", "Telsen", "Cushamen", "Futaleufú", "Gaiman", "Rawson", "Languiñeo", "Mártires", "Paso de Indios", "Tehuelches", "Florentino Ameghino", "Río Senguer", "Escalante", "Sarmiento"),
+  name = c("Biedma", "Gastre", "Telsen",
+           "Cushamen", "Futaleufú", "Gaiman",
+           "Rawson", "Languiñeo", "Mártires", 
+           "Paso de Indios", "Tehuelches", "Florentino Ameghino",
+           "Río Senguer", "Escalante", "Sarmiento"),
+  code = c("002", "004", "003",
+            "005", "006", "012",
+            "009", "008", "011",
+            "013", "015", "014"),
   row = c(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4),
   col = c(5, 3, 4, 2, 1, 3, 4, 2, 3, 2, 1, 4, 1, 3, 2),
   stringsAsFactors = FALSE
